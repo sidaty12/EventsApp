093203dd2d588440a70802dc6e84f89df4605e74
SidatyCheikh <SidatyCheikh@WEBATRIOCONCAS>
2024-02-09 11:19:01 +0100

    Ajoutez des fichiers projet.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EventsApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
